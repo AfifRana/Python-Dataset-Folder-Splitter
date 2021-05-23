0191d2bab1fb2a74d7b663634c147a89c52b39ad
Afif Rana Muhammad <36437450+AfifRana@users.noreply.github.com>
2021-05-23 20:29:46 +0700

    For Image + Annotation.txt

---
+++ split-folder-dataset_v2.py
@@ -0,0 +1,125 @@
+import os, os.path, shutil, math
+
+# your path
+source_path = '/content/data/' # change the value
+destination_path = '/content/data-2' # change the value
+
+# percentage of dataset division for train_set, val_set, test_set
+train_div = 0.68 # change the value
+val_div = 0.17 # change the value
+test_div = 0.15 # change the value
+
+print("source path : ",source_path)
+print("destination path : ",destination_path,"\n")
+
+# count files
+#sum_files = len([name for name in os.listdir(source_path) if os.path.isfile(os.path.join(source_path, name))])
+#print(sum_files)
+
+# create directories
+path = destination_path+"/train" # your path
+os.makedirs(path)
+path = destination_path+"/val" # your path
+os.makedirs(path)
+path = destination_path+"/test" # your path
+os.makedirs(path)
+
+# save all of filenames in list, then sort it asc
+myList = os.listdir(source_path)
+myList.sort()
+sumImages = int(len(myList)/2)
+print("sum of your files : ",len(myList))
+print("sum of your images : ",sumImages)
+
+print("\nyour division \ntrain_div : ",train_div,"\nval_div : ",val_div,"\ntest_div : ",test_div)
+print("percentage of dataset division",train_div+val_div+test_div,"\n")
+
+if (train_div+val_div+test_div != 1.0):
+  raise ValueError("Must be 1.0")
+
+# sum of files in each set
+lenList = len(myList)
+count_train = sumImages*train_div
+count_val = sumImages*val_div
+count_test = sumImages*test_div
+
+print("\nImage Files\n")
+
+print("sumImages : ",sumImages)
+print("count_train : ",count_train)
+print("count_val : ",count_val)
+print("count_test : ",count_test)
+
+print("\nImage with Annotations\n")
+
+print("lenList : ",lenList)
+print("count_train : ",count_train*2)
+print("count_val : ",count_val*2)
+print("count_test : ",count_test*2)
+
+print("\nMod\n")
+
+mod_count_train = math.ceil(count_train)
+mod_count_val = math.floor(count_val)
+mod_count_test = math.floor(count_test)
+sum_mod = mod_count_train+mod_count_val+mod_count_test
+
+print("count_train : ",mod_count_train)
+print("count_val : ",mod_count_val)
+print("count_test : ",mod_count_test)
+print("sum_mod : ",sum_mod)
+
+print("\nUpdate Mod\n")
+
+if(sum_mod<sumImages):
+  mod_count_train += sumImages - sum_mod
+  sum_mod = mod_count_train+mod_count_val+mod_count_test
+
+print("count_train : ",mod_count_train)
+print("count_val : ",mod_count_val)
+print("count_test : ",mod_count_test)
+print("sum_mod : ",sum_mod)
+
+print("\nMod with Annotation\n")
+
+print("count_train : ",mod_count_train*2)
+print("count_val : ",mod_count_val*2)
+print("count_test : ",mod_count_test*2)
+print("sum_mod : ",sum_mod*2)
+
+len_train = mod_count_train*2
+len_val = len_train + (mod_count_val*2)
+len_test = len_val + (mod_count_test*2)
+
+print("\nMod len\n")
+
+print("len_train : ",len_train)
+print("len_val : ",len_val)
+print("len_test : ",len_test)
+
+# moving files
+list_train = []
+list_val = []
+list_test = []
+
+for i in range(len_train):
+	list_train.append(myList[i])
+
+for i in range(len_train,len_val):
+	list_val.append(myList[i])
+
+for i in range(len_val,len_test):
+	list_test.append(myList[i])
+
+print(list_train,"\n sum :",len(list_train))
+print(list_val,"\n sum :",len(list_val))
+print(list_test,"\n sum :",len(list_test))
+
+for f in list_train:
+  shutil.copy(source_path+f, destination_path+'/train') # your path
+
+for f in list_val:
+  shutil.copy(source_path+f, destination_path+'/val') # your path
+
+for f in list_test:
+  shutil.copy(source_path+f, destination_path+'/test') # your path
